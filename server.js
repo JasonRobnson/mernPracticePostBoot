e280ee8e212e098281a6e26b9a014c261f98a23a
unknown <bubbastinx@gmail.com>
2018-08-22 20:17:55 -0500

    built out User model

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app = express();
 const mongoose = require('mongoose');
 const auth = require('./routes/api/auth.js');
 const profile = require('./routes/api/profile.js');
-const posts = require('./routes/api/posts.js')
-
+const posts = require('./routes/api/posts.js');
+const bodyParser = require('body-parser');
 const db = require('./config/keys').mongoURI;
+
+//Body Parser middleware 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //CONNECT  TO MONGODB
 mongoose
