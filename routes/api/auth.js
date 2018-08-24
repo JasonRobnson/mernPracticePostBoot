fdcb3686630a2ad99b4bce1db71ecc275031100c
unknown <bubbastinx@gmail.com>
2018-08-24 09:01:23 -0500

    created validation for register route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,7 +7,10 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys')
-const passport = require('passport')
+const passport = require('passport');
+
+//Load Input Validation 
+const validateRegisterInput = require('../../validation/register')
 
 //@route Get api/auth/test
 //@desc Test auth route
@@ -18,12 +21,21 @@ router.get('/test', (req, res) => res.json({msg: "Auth Works"}));
 //@desc Register auth route
 //@access Public
 router.post('/register', (req, res) => {
+ 
+  //Destructuring
+  const { errors, isValid } = validateRegisterInput(req.body);
+  //check validation
+  if(!isValid) {
+    return res.status(400).json(errors)
+  }
+
   User.findOne({
     email: req.body.email
   })
   .then(user => {
     if(user) {
-      return res.status(400).json({email: 'Email already exists'})
+      errors.email = 'Email already exists'
+      return res.status(400).json({errors})
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', // size
