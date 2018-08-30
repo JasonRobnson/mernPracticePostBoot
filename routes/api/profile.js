615e9da06756039eab57819cb3d914106984c5da
unknown <bubbastinx@gmail.com>
2018-08-29 20:05:23 -0500

    developed post routes for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -13,7 +13,7 @@ const User= require('../../models/User');
 //@access Public
 router.get('/test', (req, res) => res.json({msg:'Profile Works'}));
 
-//@route Get api/profile/test
+//@route Get api/profile
 //@desc Get Current User Profile
 //@access Private
 router.get('/', passport.authenticate('jwt', { session: false}), (req, res) => {
@@ -28,5 +28,34 @@ router.get('/', passport.authenticate('jwt', { session: false}), (req, res) => {
   })
   .catch( err => res.status(404).json(err));
 });
+
+//@route Get api/profile
+//@desc Create -or edit User Profile
+//@access Private
+router.post('/', passport.authenticate('jwt', { session: false}), (req, res) => {
+  
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if(req.body.handle) profileFields.handle = req.body.handle;
+  if(req.body.company) profileFields.company = req.body.company;
+  if(req.body.location) profileFields.location = req.body.location;
+  if(req.body.bio) profileFields.bio = req.body.bio;
+  if(req.body.status) profileFields.status = req.body.status;
+  if(req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+  //Skill split into array
+  if(typeof req.body.skills !== 'undefined') {
+    profileFields.skills = req.body.skills.split(',');
+  }
+  profileFields.social = {}; 
+  if(req.body.handle) profileFields.social.youtube = req.body.youtube;
+  if(req.body.twitter) profileFields.social.twitter = req.body.twitter;
+  if(req.body.facebook) profileFields.social.facebook = req.body.facebook;
+  if(req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+  if(req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+  Profile.findOne({ user: req.user.id })
+  
+
+ });
 
 module.exports = router;
