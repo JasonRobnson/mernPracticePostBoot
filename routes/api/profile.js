1627bc189d6416dc54254c35e823e9e046da65a9
unknown <bubbastinx@gmail.com>
2018-09-14 09:13:04 -0500

    implementing education validation, need to add from next iteration

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
-const validateExperienceInput = require('../../validation/experience')
+const validateExperienceInput = require('../../validation/experience');
+const validateEducationInput = require('../../validation/education');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
@@ -169,9 +170,37 @@ router.post('/experience', passport.authenticate('jwt', { session: false }), (re
   })
 })
 
- //@route POST  api/profile/experience
-//@desc Create experience for profile
+ //@route POST  api/profile/education
+//@desc Create education for profile
 //@access Private
+router.post('/education', passport.authenticate('jwt', { session: false }), (req, res) => {
+  console.log(req.body)
+  const { errors, isValid } = validateEducationInput(req.body); 
+  console.log( req.body )
+    //Check Validation
+     if(!isValid) {
+     //return any errors
+     return res.status(400).json(errors);
+}
+  Profile.findOne({ user: req.user.id })
+  .then(profile => {
+    const newExp = {
+      school: req.body.school,
+      degree: req.body.degree,
+      fieldofstudy: req.body.fieldofstudy,
+      from: req.body.from,
+      to: req.body.to,
+      current: req.body.current,
+      description: req.body.description
+    }
+    
+    
+    //add to exp array
+    profile.education.unshift(newExp);
+      
+    profile.save().then(profile => res.json(profile));
+  })
+})
 
 
 
