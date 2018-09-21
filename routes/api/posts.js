afedf5d1a6f48261968db6dbeb03ed395623c052
unknown <bubbastinx@gmail.com>
2018-09-21 08:51:55 -0500

    updated routes for post, and fixed key/pair problem on the req.body

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -16,10 +16,14 @@ router.get('/test', (req, res) => res.json({msg:'Posts Works'}));
 //@access Private
 
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+      if(!isValid) {
+        return res.status(400).json(errors);
+      }
   const newPost = new Post ({
     text: req.body.text,
     name: req.body.name,
-    avatar: req.body.name,
+    avatar: req.body.avatar,
     user: req.user.id
   });
   
