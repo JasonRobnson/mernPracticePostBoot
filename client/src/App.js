496076891314a3540aec1d7e1db01a7c36ddbabc
unknown <bubbastinx@gmail.com>
2018-10-02 09:32:57 -0500

    installed router

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,25 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+          
+          </div>
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/Register" component={Register} /> 
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
