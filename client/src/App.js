e0476b637c4ab47aec2808081ad99bd14d39fa84
unknown <bubbastinx@gmail.com>
2018-10-10 09:15:19 -0500

    created structure to implement redux for state management

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store.js';
+
 import './App.css';
+
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-          
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container" />
+            <Route exact path="/Login" component={Login} />
+            <Route exact path="/Register" component={Register} />
+            <Footer />
           </div>
-          <Route exact path="/Login" component={Login} />
-          <Route exact path="/Register" component={Register} /> 
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
