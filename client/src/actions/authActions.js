ffe276bbc832587303c9323a5ac1e5c5e6c7d42e
unknown <bubbastinx@gmail.com>
2018-10-23 08:47:04 -0500

    added reducer validation

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
-
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 //Redux Thunk allows us to pass in dispatch, and call it on the our async axios call
 export const registerUser = (userData, history) => dispatch => {
   axios
@@ -12,4 +13,37 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+//LOGIN GET USER TOKEN
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/auth/login', userData)
+    .then(res => {
+      //save to local storage
+      const { token } = res.data;
+      // set token to localStorage
+      localStorage.setItem('jwtToken', token);
+      // set token to authHeader
+      setAuthToken(token);
+      //Decode token to get user data
+      const decoded = jwt_decode(token);
+      //Set Current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
