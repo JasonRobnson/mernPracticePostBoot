3e23349fa0c4b24577e2b78e17026669a89dcb3e
unknown <bubbastinx@gmail.com>
2018-11-20 06:20:59 -0600

    installed spinner for dashboard loading, and handle lading conditionals

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,15 +2,49 @@ import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
-
+import Spinner from '../common/spinner';
+import { Link } from 'react-router-dom';
 class Dashboard extends Component {
   componentDidMount() {
     this.props.getCurrentProfile();
   }
   render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+
+    let dashboardContent;
+    const returnDasboardStatus = (profile, loading) => {
+      return profile == null || loading
+        ? (dashboardContent = <Spinner />)
+        : Object.keys(profile).length > 0
+        ? (dashboardContent = <h4>Display Profile: TODO</h4>)
+        : (dashboardContent = (
+            <div>
+              <p className="lead text-muted">Welcome {user.name}</p>
+              <p>You haven't set up a profile, please add some info</p>
+              <Link to="/create-profile" className="btn btn-lg btn-info">
+                Create Profile
+              </Link>
+            </div>
+          ));
+    };
+    returnDasboardStatus(profile, loading);
+    //   if(profile == null || loading) {
+    //     dashboardContent = <h4> Loading...<h4>
+    // }  else { (dashboardContent = <h1>Hello</h1>)
+    //   dashboardContent = <h1>Hello</h1>
+    // }
+
     return (
-      <div>
-        <h1>Dashboard</h1>
+      <div className="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4">Dashboard</h1>
+              {dashboardContent}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
