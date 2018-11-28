e319ed6ac70a92030795794b25ff1507be0310d2
unknown <bubbastinx@gmail.com>
2018-11-27 20:15:18 -0600

    setting up state for createprofile module

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 class CreateProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displaySocialInputs: false,
+      handle: '',
+      company: '',
+      website: '',
+      location: '',
+      status: '',
+      skills: '',
+      githubusername: '',
+      bio: '',
+      twitter: '',
+      facebook: '',
+      linkedin: '',
+      youtube: '',
+      instagram: '',
+      errors: {}
+    };
+  }
   render() {
     return <div />;
   }
