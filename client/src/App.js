26a22073e3aa4be2995a87dea491a22d57e438c8
unknown <bubbastinx@gmail.com>
2018-11-28 05:47:00 -0600

    created link to private route for create profile component

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/common/privateRoute';
+import CreateProfile from './components/create-profile/CreateProfile';
 
 import './App.css';
 
@@ -50,7 +51,14 @@ class App extends Component {
             <Route exact path="/Login" component={Login} />
             <Route exact path="/Register" component={Register} />
             <Switch>
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/Dashboard" component={Dashboard} />
+            </Switch>
+            <Switch>
+              <PrivateRoute
+                exact
+                path="/Create-profile"
+                component={CreateProfile}
+              />
             </Switch>
             <Footer />
           </div>
